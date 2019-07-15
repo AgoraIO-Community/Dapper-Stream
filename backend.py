e667a67611c85eb0b0035a01928d31b878f9d760
Anshuman Agarwal <dev.anshuman73@gmail.com>
2019-07-15 12:15:41 +0530

    Init

---
+++ backend.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import os
+
+subscription_key = os.environ.get('AZURE_KEY')
+assert subscription_key
+
+face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
+
+image_url = 'https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg'
+
+headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+
+params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'emotion',
+}
+
+response = requests.post(face_api_url, params=params,
+                         headers=headers, json={"url": image_url})
+print(json.dumps(response.json()))
